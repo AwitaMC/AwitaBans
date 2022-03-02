50576c869475d024f1e86ad98352312462f4cdba
Raúl M <between13131@gmail.com>
2022-03-02 07:11:19 +0100

    Incluír hash del commit en footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,6 @@
+<footer>
+    <p>AwitaBans by BetTD - <code><?php exec('git log --pretty="%h" -n1 HEAD'); ?></code></p>
+</footer>
 <script src="<?php echo $this->resource('inc/js/jquery-3.5.1.min.js'); ?>"></script>
 <script src="<?php echo $this->resource('inc/js/bootstrap.min.js'); ?>"></script>
 <?php echo "</html>"; ?>
