bc754b46086a928bc841feca0d86650c81656263
Raúl M <between13131@gmail.com>
2022-03-02 07:22:35 +0100

    Añadir nota de litebans-php en footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,6 @@
 <footer>
     <p>AwitaBans by BetTD - <code><?= exec('git log --pretty="%h" -n1 HEAD'); ?></code></p>
+    <p>Un proyecto basado en <a href="https://gitlab.com/ruany/litebans-php" target="_blank">litebans-php</a> de Ruan</p>
 </footer>
 <script src="<?php echo $this->resource('inc/js/jquery-3.5.1.min.js'); ?>"></script>
 <script src="<?php echo $this->resource('inc/js/bootstrap.min.js'); ?>"></script>
