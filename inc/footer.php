8b70aa0ee56e1a87d2b77a21cb6d5acc4e64835c
Raúl M <between13131@gmail.com>
2022-03-02 07:14:02 +0100

    Fix: no incluye hash xD

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <p>AwitaBans by BetTD - <code><?php exec('git log --pretty="%h" -n1 HEAD'); ?></code></p>
+    <p>AwitaBans by BetTD - <code><?= exec('git log --pretty="%h" -n1 HEAD'); ?></code></p>
 </footer>
 <script src="<?php echo $this->resource('inc/js/jquery-3.5.1.min.js'); ?>"></script>
 <script src="<?php echo $this->resource('inc/js/bootstrap.min.js'); ?>"></script>
