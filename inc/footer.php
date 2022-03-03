bb8eff523656833f34db37c9adb6de205da860ac
Raúl M <between13131@gmail.com>
2022-03-03 05:35:39 +0100

    Cambiar estilo de footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,10 @@
 <footer>
-    <p>AwitaBans by BetTD - <code><?= exec('git log --pretty="%h" -n1 HEAD'); ?></code></p>
-    <p>Un proyecto basado en <a href="https://gitlab.com/ruany/litebans-php" target="_blank">litebans-php</a> de Ruan</p>
+    <div class="container">
+        <div class="acrylic">
+            <p>AwitaBans by BetTD - <code><?= exec('git log --pretty="%h" -n1 HEAD'); ?></code></p>
+            <p>Un proyecto basado en <a href="https://gitlab.com/ruany/litebans-php" target="_blank">litebans-php</a> de Ruan</p>
+        </div>
+    </div>
 </footer>
 <script src="<?php echo $this->resource('inc/js/jquery-3.5.1.min.js'); ?>"></script>
 <script src="<?php echo $this->resource('inc/js/bootstrap.min.js'); ?>"></script>
